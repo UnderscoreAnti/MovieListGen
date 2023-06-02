f9d3ac6f7be9bb8f4fd2e1391dd00dcd5ba70980
UnderscoreAnti <UnderscoreAnti@gmail.com>
2023-06-02 09:29:35 -0600

    Ensured unique IDs for each movie with changes to TexttoDB file and we had a movie night so those changes had to be made too.

--- TxtToDB.py
+++ TxtToDB.py
@@ -17,8 +17,20 @@ def process_txt(route):
     return final_arr
 
 
+def create_id(check_arr):
+    ran.seed()
+    new_id = ran.randint(0, 2147483647)
+    new_id = int(new_id)
+
+    while new_id in check_arr:
+        new_id = ran.randint(0, 2147483647)
+        new_id = int(new_id)
+
+    return new_id
+
+
 if __name__ == '__main__':
-    testID = 0
+    id_arr = []
     new_conn = sqlite3.connect("SaveFile.db")
     data_cursor = new_conn.cursor()
 
@@ -28,11 +40,10 @@ if __name__ == '__main__':
     out_list = process_txt(route_one)
 
     for entry in out_list:
-        ran.seed()
         rank = out_list.index(entry) + 1
-        new_id = ran.randint(0, 2147483647)
-        new_id = int(new_id)
-        entry_data = (new_id, 1, 1, "Movie not rejected", "Movie not reviewed", rank, rank, rank, rank, entry)
+        mov_id = create_id(id_arr)
+        id_arr.append(mov_id)
+        entry_data = (mov_id, 1, 1, "Movie not rejected", "Movie not reviewed", rank, rank, rank, rank, entry)
         data_cursor.execute("""INSERT INTO movies VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)""", entry_data)
         new_conn.commit()
 
@@ -40,15 +51,14 @@ if __name__ == '__main__':
     print("\nend route one\n")
 
     out_list_two = process_txt(route_two)
+
     for entry in out_list_two:
-        ran.seed()
-        new_id = ran.randint(0, 2147483647)
-        new_id = int(new_id)
-        entry_data = (new_id, 0, 0, "Movie not rejected", "Movie not yet reviewed", 0, 0, 0, 0, entry)
+        mov_id = create_id(id_arr)
+        id_arr.append(mov_id)
+        entry_data = (mov_id, 0, 0, "Movie not rejected", "Movie not yet reviewed", 0, 0, 0, 0, entry)
         data_cursor.execute("""INSERT OR REPLACE INTO movies VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)""", entry_data)
         new_conn.commit()
 
-    #new_conn.commit()
     new_conn.close()
 
 
